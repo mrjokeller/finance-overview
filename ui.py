291302338905b3e1ce6fbf3542dc30848bb51959
Jonathan Keller <jonikeller98@gmail.com>
2023-03-29 18:46:11 +0200

    set minsize and distrubute columns equally

--- ui.py
+++ ui.py
@@ -11,8 +11,13 @@ class UI:
         self.window = tk.Tk()
         self.window.title("Finanzen")
         self.window.geometry("500x500")
+        self.window.minsize(300, 450)
         
         self.window.config(padx=20, pady=20)
+        
+        self.window.grid_columnconfigure(0, weight=2, uniform="fred")
+        self.window.grid_columnconfigure(1, weight=1, uniform="fred")
+        self.window.grid_columnconfigure(2, weight=1, uniform="fred")
         
         # Dropdown menu
         countries = [country.title() for country in countries]
@@ -20,34 +25,39 @@ class UI:
         self.country_name.trace_add('write', self.update_expenses)
         self.country_name.set(countries[0])
         self.dropdown = tk.OptionMenu(self.window, self.country_name, *countries)
-        self.dropdown.grid(row=0, column=0, sticky='w', pady=20)
+        self.dropdown.grid(row=0, column=0, sticky='w', pady=10)
         self.dropdown.config(width=8)
-        
-        
-        # Labels with total cost, total planned cost and total difference
-        self.total_cost_label = tk.Label(self.window, text="Total cost: ", pady=20)
-        self.total_cost_label.grid(row=1, column=0, sticky='w')
-        self.total_planned_cost_label = tk.Label(self.window, text="Total planned cost: ")
-        self.total_planned_cost_label.grid(row=2, column=0, sticky='w')
-        self.total_difference_label = tk.Label(self.window, text="Total difference: ")
-        self.total_difference_label.grid(row=1, column=2, sticky='w')
-        
-        # Cost labels with actual expenses and planned expenses
-        self.total_cost = tk.Label(self.window, text="", pady=20)
-        self.total_cost.grid(row=1, column=1, sticky='e')
-        self.total_planned_cost = tk.Label(self.window, text="")
-        self.total_planned_cost.grid(row=2, column=1, sticky='e')
         
         # create a separator
         separator = ttk.Separator(self.window, orient='horizontal')
-        separator.grid(row=3, column=0, columnspan=3, sticky='ew', pady=20)
+        separator.grid(row=1, column=0, columnspan=3, sticky='ew', pady=20)
         
         self.subheading_label = tk.Label(self.window, text="Categories", font=("Arial", 16))
-        self.subheading_label.grid(row=4, column=0, sticky='w')
+        self.subheading_label.grid(row=2, column=0, sticky='w')
         self.subheading_label_2 = tk.Label(self.window, text="Actual", font=("Arial", 16))
-        self.subheading_label_2.grid(row=4, column=1, sticky='w')
+        self.subheading_label_2.grid(row=2, column=1, sticky='e')
         self.subheading_label_3 = tk.Label(self.window, text="Planned", font=("Arial", 16))
-        self.subheading_label_3.grid(row=4, column=2, sticky='e')
+        self.subheading_label_3.grid(row=2, column=2, sticky='e')
+        
+        # create a separator
+        separator = ttk.Separator(self.window, orient='horizontal')
+        separator.grid(row=9, column=0, columnspan=3, sticky='ew', pady=20)
+        
+        # Labels with total cost, total planned cost and total difference
+        self.total_cost_label = tk.Label(self.window, text="Total:", font=("Arial", 16))
+        self.total_cost_label.grid(row=10, column=0, sticky='w')
+        self.total_difference_label = tk.Label(self.window, text="Total difference: ", font=("Arial", 16))
+        self.total_difference_label.grid(row=11, column=0, sticky='w')
+        
+        # Cost labels with actual expenses and planned expenses
+        self.total_cost = tk.Label(self.window, text="", pady=20)
+        self.total_cost.grid(row=10, column=1, sticky='e')
+        self.total_planned_cost = tk.Label(self.window, text="")
+        self.total_planned_cost.grid(row=10, column=2, sticky='e')
+        
+        # Difference cost
+        self.difference = tk.Label(self.window, text="", pady=20)
+        self.difference.grid(row=11, column=2, sticky='e')
 
         self.update_expenses()
         
@@ -57,14 +67,14 @@ class UI:
     def update_expenses(self, *args):
         selected_country = self.country_name.get().lower()
         
-        # Update total cost labels
+        # Update overview costs
         total_cost_actual = self.databases[selected_country].get_total_cost()
         total_cost_planned = self.databases[selected_country].get_total_cost(is_planned=True)
         difference = total_cost_planned - total_cost_actual
         
-        self.total_cost_label.config(text=f"Total cost: {total_cost_actual:.2f} €")
-        self.total_planned_cost_label.config(text=f"Total planned cost: {total_cost_planned:.2f} €")
-        self.total_difference_label.config(text=f"Difference: {difference:.2f} €", fg="red" if difference < 0 else "green")
+        self.total_cost.config(text=f"{total_cost_actual:.2f} €")
+        self.total_planned_cost.config(text=f"{total_cost_planned:.2f} €")
+        self.difference.config(text=f"{difference:.2f} €", fg="red" if difference < 0 else "green")
         
         
         # Remove existing category labels
@@ -79,7 +89,7 @@ class UI:
         for i, category in enumerate(self.categories):
             # Category Label
             label = tk.Label(self.window, text=category.capitalize())
-            label.grid(row=i+5, column=0, sticky='w')
+            label.grid(row=i+3, column=0, sticky='w')
             self.category_labels[category] = label
             
             # Actual cost label
@@ -88,7 +98,7 @@ class UI:
             except KeyError:
                 actual_label = tk.Label(self.window, text="0.00")
             
-            actual_label.grid(row=i+5, column=1, sticky='e')
+            actual_label.grid(row=i+3, column=1, sticky='e')
             self.category_labels[f"{category}_actual"] = actual_label
             
             # Planned cost label
@@ -96,6 +106,6 @@ class UI:
                 planned_label = tk.Label(self.window, text=categories_planned[category])
             except KeyError:
                 planned_label = tk.Label(self.window, text="0.00")
-            planned_label.grid(row=i+5, column=2, sticky='e')
+            planned_label.grid(row=i+3, column=2, sticky='e')
             self.category_labels[f"{category}_planned"] = planned_label
 
